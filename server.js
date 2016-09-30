67c69c651613e4d886ccec8b2bbeb9a9568478e2
vivekagal1998 <vivekagal1998@gmail.com>
2016-09-30 17:28:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get ('/article-one',function (req,res){
-    res.send ('Vivek Coding is ur passion');
+    res.sendFile (path.join (__dirname,'ui','a1.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
