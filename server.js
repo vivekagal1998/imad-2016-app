1865cd16a0fe063fe2db698101d8e067e4131c50
vivekagal1998 <vivekagal1998@gmail.com>
2016-12-19 11:51:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', function (req, res) {
 app.get ('/article-one',function (req,res){
     res.sendFile (path.join (__dirname,'ui','a1.html'));
 });
+app.get ('/article-two',function (req,res){
+    res.sendFile (path.join (__dirname,'ui','a1.html'));
+});
+app.get ('/article-three',function (req,res){
+    res.sendFile (path.join (__dirname,'ui','a1.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
